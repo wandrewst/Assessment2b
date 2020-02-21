3298c6367b4e86d4ab8630a4bb0c279ab11c1e32
Winnie R Davis <41178060+wandrewst@users.noreply.github.com>
2020-02-20 20:07:39 -0500

    Add files via upload

---
+++ Assessment2b/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Assessment2b
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Challenge.CreateArray();
+        }
+    }
+}
---
+++ Challenge.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assessment2b
+{
+    class Challenge
+    {
+        static void CreateArray (string message)
+        {
+            string phrase = "The quick brown    fox     jumps over the lazy dog.";
+            string[] words = phrase.Split(' ');
+
+            foreach (var word in words)
+            {
+                System.Console.WriteLine($"<{word}>");
+            }
+
+        }
+
+       
+    }
+}
